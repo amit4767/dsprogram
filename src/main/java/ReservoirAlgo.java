3f1d16b40a73685e600122eba5f6e212ee1a78c1
Kumar, Amit <Amit.Kumar@cdk.com>
2019-01-10 10:15:44 +0530

    Reservoir Aglo

---
+++ src/main/java/ReservoirAlgo.java
@@ -0,0 +1,5 @@
+public class ReservoirAlgo {
+
+    //Task is to design an algorithm thats capable of selecting k items from an array!
+    // The problem is that the n size of the array is unknown (or infinitely large)!
+}
