f95990176bd0a122fb01ed8f1b96e2181a196e7b
Kumar, Amit <Amit.Kumar@cdk.com>
2019-01-10 10:09:51 +0530

    update algo for find dupes in array

--- src/main/java/DupesInArray.java
+++ src/main/java/DupesInArray.java
@@ -4,5 +4,25 @@ public class DupesInArray {
     // T[] one-dimensional array of integers in O(N) running time where the
     // integer values are smaller than the length of the array!
 
+    public void findDupes(int[] nums){
+        if(nums.length !=0 ){
+            int arraryLength = nums.length;
+               for(int i =0 ; i< arraryLength ; i++){
+                   int temp = nums[i];
+                   int tempmode = Math.abs(temp);
+                   int nextValue = nums[tempmode];
+                   if(nextValue > 0){
+                       nums[tempmode] = -nextValue;
+                   }else {
+                       System.out.println("this value is dupes ="+tempmode);
+                   }
+               }
+        }else{
+
+            System.out.println("This is empty string Tiger :)");
+        }
+    }
+
+
 
 }
--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -1,7 +1,15 @@
 public class Helloworld {
 
     public static void main(String[] args) {
+        int [] arr  = new int[6];
+        arr[0] =1;
+        arr[1] =2;
+        arr[2] =5;
+        arr[3] =3;
+        arr[4] =4;
+        arr[5] =5;
 
-        System.out.println("Hello world first check in ");
+
+        new DupesInArray().findDupes(arr);
     }
 }
