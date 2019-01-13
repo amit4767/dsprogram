34bb4562bf78c34c154a274f9ea76359f1db4c76
Kumar, Amit <Amit.Kumar@cdk.com>
2019-01-13 14:07:31 +0530

    giving hint.what not work

--- src/main/java/ReservoirAlgo.java
+++ src/main/java/ReservoirAlgo.java
@@ -2,4 +2,9 @@ public class ReservoirAlgo {
 
     //Task is to design an algorithm thats capable of selecting k items from an array!
     // The problem is that the n size of the array is unknown (or infinitely large)!
+
+    //hell
+
+    //it would very easy to select any random number if knw the size of array
+    //i.e new Randon.nextInt(lengthofArray)
 }
