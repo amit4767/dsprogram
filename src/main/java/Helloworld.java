6a8cb30cb506f73e48003ab33a2c1643a8bec9ea
Kumar, Amit <Amit.Kumar@cdk.com>
2019-01-14 09:38:20 +0530

    added logic for random selection of arrray.

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -9,7 +9,16 @@ public class Helloworld {
         arr[4] =4;
         arr[5] =5;
 
-
+         // Testing for dupes in array
         new DupesInArray().findDupes(arr);
+
+        int bigArry[]  ={1,3,5,6,8,9 ,10,15,16,18,19,20,21,22, 34,34};
+
+         // testing for random select in array
+        new ReservoirAlgo().randonSelet(bigArry,5);
+
     }
+
+
+
 }
--- src/main/java/ReservoirAlgo.java
+++ src/main/java/ReservoirAlgo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ReservoirAlgo {
 
     //Task is to design an algorithm thats capable of selecting k items from an array!
@@ -7,4 +9,40 @@ public class ReservoirAlgo {
 
     //it would very easy to select any random number if knw the size of array
     //i.e new Randon.nextInt(lengthofArray)
+
+    private Random random;
+
+    public  ReservoirAlgo(){
+         this.random = new Random();
+    }
+
+
+    public  void randonSelet(int [] num , int k){
+
+        //create array to select random k item
+      int [] reservoir = new int[k];
+
+      //copied starting 5 element from big to sample array
+      for(int i = 0 ; i <k  ; i ++)
+          reservoir[i] = num[i];
+
+
+
+      //k +1 because we have already copied k element
+
+      for(int i = k+1 ; i < num.length  ; i++){
+
+          //the ith element chosen to be include with probability k/i
+          int j = random.nextInt(i) +1;
+          if(j < k)  reservoir[j]  = num[i];
+
+      }
+
+      System.out.print("Random selected array");
+      for(int i=0 ;i < reservoir.length  ;i++){
+          System.out.print(" "+reservoir[i]);
+      }
+
+    }
+
 }
