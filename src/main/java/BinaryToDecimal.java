198f9b6de5e8e764261f9c6dc0247817c298d9d1
kumaram <>
2019-05-05 12:20:43 +0530

    added two method for bin to Int

--- src/main/java/BinaryToDecimal.java
+++ src/main/java/BinaryToDecimal.java
@@ -1,31 +1,37 @@
 public class BinaryToDecimal {
 
 
-    public  int binToInt(String bin){
+    private int binToInt(String bin) {
 
-        int conversion=1;
-        int result =0;
-
-
-        for(int i=1 ; i <= bin.length() ;i++){
-
+        int conversion = 1;
+        int result = 0;
+        for (int i = 1; i <= bin.length(); i++) {
             //we need to take last char first
-
-            if(bin.charAt(bin.length() - i) =='1'){
-
+            if (bin.charAt(bin.length() - i) == '1')
                 result += conversion;
-            }
-              conversion *= 2;
+            conversion *= 2;
+        }
+        return result;
+    }
 
+    private int binToIntAlt(String bin) {
+
+        int result = 0;
+
+        for (int i = 1; i <= bin.length(); i++) {
+            //we need to take last char first
+            if (bin.charAt(bin.length() - i) == '1')
+                result += Math.pow(2, i - 1);
         }
 
-        return  result;
+        return result;
     }
 
     public static void main(String[] args) {
 
         BinaryToDecimal btd = new BinaryToDecimal();
-        System.out.println(" Result = "+btd.binToInt("10110"));
+        System.out.println(" Result = " + btd.binToInt("10110"));
+        System.out.println(" Result = " + btd.binToIntAlt("10110"));
     }
 
 }
