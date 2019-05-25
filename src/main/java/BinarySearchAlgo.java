5b75b9d5d79109715820d9bc0b61417f321865ad
kumaram <>
2019-05-25 17:31:42 +0530

    simple intersection

---
+++ src/main/java/BinarySearchAlgo.java
@@ -0,0 +1,18 @@
+public class BinarySearchAlgo {
+
+
+    public  boolean findNumber(int a , int [] arr){
+
+
+
+
+        return  false;
+    }
+
+    public static void main(String[] args) {
+
+        BinarySearchAlgo binarySearchAlgo = new BinarySearchAlgo();
+
+        System.out.println(binarySearchAlgo.findNumber(7 ,new int[] {1,2,3,4,5,6,7,8,9}));
+    }
+}
