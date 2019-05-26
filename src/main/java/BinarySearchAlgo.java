0426ab90aafc791099d42ba98b83233ec6f12019
kumaram <>
2019-05-26 17:42:53 +0530

    update Binany search

--- src/main/java/BinarySearchAlgo.java
+++ src/main/java/BinarySearchAlgo.java
@@ -1,11 +1,20 @@
 public class BinarySearchAlgo {
 
-
     public  boolean findNumber(int a , int [] arr){
+       int starpoint = 0;
+       int endPoint = arr.length -1;
+       while(starpoint <= endPoint){
+           System.out.println("startponit = "+starpoint);
+           System.out.println("endPoint = "+endPoint);
+           int midpoint = (endPoint - starpoint)/2 + starpoint;
+           if(arr[midpoint] == a ) return  true;
+           if(arr[midpoint] > a) {
+               endPoint = midpoint;
+           }else {
+               starpoint = midpoint +1;
+           }
 
-
-
-
+       }
         return  false;
     }
 
@@ -13,6 +22,6 @@ public class BinarySearchAlgo {
 
         BinarySearchAlgo binarySearchAlgo = new BinarySearchAlgo();
 
-        System.out.println(binarySearchAlgo.findNumber(7 ,new int[] {1,2,3,4,5,6,7,8,9}));
+        System.out.println(binarySearchAlgo.findNumber(10,new int[] {1,2,3,4,5,6,7,8,9}));
     }
 }
