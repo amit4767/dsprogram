f0cb26b3cee204b7770c384ff80b5b017c570553
kumaram <>
2019-07-20 18:36:46 +0530

    formatted

--- src/main/java/com/cdk/ds/union/FibonacciClass.java
+++ src/main/java/com/cdk/ds/union/FibonacciClass.java
@@ -9,31 +9,21 @@ public class FibonacciClass {
      }
 
      public  FibonacciClass(int n ){
-
          lookup = new int[n +1];
-
          for (int i=0 ; i <= n ; i++)
                lookup[i] =-1;
      }
+
     public  int fibMem(int n){
-
-
          if(lookup[n]  == -1){
-
              if(n <= 1)
                  lookup[n] =n;
              else
-
            lookup[n] = fibMem(n-1) + fibMem(n-2);
 
          }
-
              return  lookup[n];
-
-
-
     }
-
     public  int fib(int n){
         if(n<=1)
             return  n;
@@ -41,17 +31,11 @@ public class FibonacciClass {
 
     }
     public static void main(String[] args) {
-
         System.out.println("Hello Fibonacci");
-
         FibonacciClass fc = new FibonacciClass();
         System.out.println(fc.fib(8));
-
         FibonacciClass fc2 = new FibonacciClass(8);
         System.out.println( fc2.fibMem(8));
-
-
     }
-
 
 }
