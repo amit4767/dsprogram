499c28c0829b3e9ccee7ba6ff6c14dd9038d33ec
Kumar, Amit <Amit.Kumar@cdk.com>
2020-08-01 12:58:05 +0530

    updated Sorted Algo

--- src/main/java/com/cdk/ds/day1/BubbleAlgo.java
+++ src/main/java/com/cdk/ds/day1/BubbleAlgo.java
@@ -2,16 +2,25 @@ package com.cdk.ds.day1;
 
 public class BubbleAlgo {
 
-    public int [] SortArray(int [] arr){
-
+    public int[] SortArray(int[] arr) {
         int len = arr.length;
+        int flag = 1;
+        for (int i = 0; i < len - 1 && flag == 1; i++) {
+            flag = 0;
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = 1;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-        for(int i=0;i<len ;i ++){
+                }
+            }
 
 
         }
 
 
-         return  new int[]{1,2,3,4,5};
+        return arr;
     }
 }
