3a083b257a4f043ebbca6c078c8202c3f64ab894
Kumar, Amit <Amit.Kumar@cdk.com>
2020-08-29 19:18:12 +0530

    update Thread name from executor service using Named factory

--- src/main/java/com/testcompany/ds/threadname/NamingThreadFirstTech.java
+++ src/main/java/com/testcompany/ds/threadname/NamingThreadFirstTech.java
@@ -1,5 +1,7 @@
 package com.testcompany.ds.threadname;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class NamingThreadFirstTech {
@@ -9,23 +11,30 @@ public class NamingThreadFirstTech {
         String mainThread = Thread.currentThread().getName();
         System.out.println("Main  thread star here = "+mainThread);
 
-        new Thread( new LoopTaskB(),"Woker-1").start();
+//        new Thread( new LoopTaskB(),"Woker-1").start();
+//
+//        Thread t2 = new Thread(new LoopTaskB());
+//        // t2.setName("Worker-2");
+//        t2.start();
+//
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-        Thread t2 = new Thread(new LoopTaskB());
-        // t2.setName("Worker-2");
-        t2.start();
+        ExecutorService executorService  = Executors.newCachedThreadPool( new NamedThreadFactory());
 
-        try {
-            TimeUnit.MILLISECONDS.sleep(800);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for(int i= 0;i < 3 ;i++){
+
+            executorService.execute(new LoopTaskB());
         }
 
         System.out.println("Main  thread half way here  = "+mainThread);
 
-        t2.setName("Woker-2");
-
-        System.out.println("["+mainThread+ "] Main thread end here ........");
+//        t2.setName("Woker-2");
+//
+//        System.out.println("["+mainThread+ "] Main thread end here ........");
 
     }
 
