37b46d71817903527fe23650a49ddb8b63f8ce96
Kumar, Amit <Amit.Kumar@cdk.com>
2020-08-31 16:57:13 +0530

    MergeSort after checking the code.

--- src/main/java/com/testcompany/ds/day9/MainMerge.java
+++ src/main/java/com/testcompany/ds/day9/MainMerge.java
@@ -8,5 +8,11 @@ public class MainMerge {
         int [] arr = new int[] {  5 , 23 ,34 ,645 ,2344 ,22 ,32};
         MergeSort mergeSort =  new MergeSort();
         System.out.println(" Merget out put ="+ Arrays.toString(mergeSort.mergSort(arr,0 ,arr.length -1)));
+
+        MergeSortTryMyself mergeSortTryMyself = new MergeSortTryMyself();
+
+        int [] arr1 = new int[] {  5 , 23 ,34 ,645 ,2344 ,22 ,32};
+        System.out.println(" Merget out put ="+ Arrays.toString(mergeSortTryMyself.mergersort(arr1,0 ,arr.length -1)));
+
     }
 }
