e3710c66fbb1eff0f5f07e9c4b35256bd9518dd8
Kumar, Amit <Amit.Kumar@cdk.com>
2020-09-03 16:37:09 +0530

    all sorting

---
+++ src/main/java/com/testcompany/ds/day12/AllSortingTwo.java
@@ -0,0 +1,166 @@
+package com.testcompany.ds.day12;
+
+public class AllSortingTwo {
+
+    public int []  bubbleSort( int [] arr){
+
+        boolean flag  = true ;
+        for(int i = 0 ; i < arr.length && flag ; i++){
+             flag = false;
+
+           for(int j = 0 ; j <arr.length -1 -i ; j++){
+
+               if(arr[j] > arr[j+1]){
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+                   flag = true;
+               }
+           }
+
+
+        }
+
+        return  arr;
+
+    }
+
+    public  int [] insertSort(int [] arr){
+        for(int i = 1 ;i < arr.length ; i++){
+            int j = i-1;
+            int temp = arr[i];
+            while(j >= 0 && temp < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+        return  arr;
+    }
+
+    public  int [] selectionSort(int [] arr){
+
+        for(int i =0 ; i < arr.length ; i++){
+
+              int minIndex = i ;
+              int minValue = arr[i];
+
+            for(int j =i ; j< arr.length ; j++){
+
+                if(minValue > arr[j]){
+                    minValue = arr[j];
+                    minIndex = j;
+                }
+
+                if(minIndex != i){
+
+                    int temp = arr[minIndex];
+                    arr[minIndex] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+
+
+        }
+        return  arr;
+    }
+
+    public  int [] mergSort(int [] arr , int low , int high ){
+
+        if(low < high){
+
+            int mid =(low + high)/2;
+            mergSort(arr , low ,mid);
+            mergSort(arr,mid +1,high);
+            merge(arr,low ,mid ,high);
+        }
+        return  arr;
+    }
+
+    private  void merge(int [] arr , int low , int mid , int high){
+
+        int [] temparr =  new int[arr.length];
+        int i = low;
+        int k = low;
+        int j = mid+1;
+
+        while(i <=mid  &&  j <= high){
+             if(arr[i] <= arr[j]){
+                 temparr[k] = arr[i];
+                 i++;
+             }else {
+                 temparr[k] = arr[j];
+                 j++;
+             }
+
+             k++;
+        }
+
+        if( i > mid){
+
+              while(j <= high){
+
+                  temparr[k] = arr[j];
+                  j++;
+                  k++;
+              }
+
+        }else {
+
+            while (i<= mid){
+                temparr[k] = arr[i];
+                i++;
+                k++;
+            }
+        }
+
+        for(k = low; k <= high ; k++){
+            arr[k] = temparr[k];
+
+        }
+
+
+    }
+
+    public  int [] QuickSort(int [] arr , int low , int high){
+
+
+        if( low  < high){
+            int loc =  quick( arr ,  low ,  high);
+            QuickSort(arr ,low , loc -1);
+            QuickSort(arr ,loc +1 , high);
+
+        }
+        return  arr;
+    }
+
+   private int quick(int [] arr , int low , int high){
+
+        int  start = low;
+        int  pivot = arr[low];
+        int  end = high;
+
+        while(start < end){
+              while(arr[start] <= pivot ){
+                  start ++;
+              }
+              while (arr[end] > pivot){
+                  end --;
+              }
+
+            if(start < end){
+                int temp = arr[end];
+                arr[end] = arr[start];
+                arr[start] =temp;
+
+            }
+        }
+
+        int temp = arr[end];
+        arr[end] = arr[low];
+        arr[low] = temp;
+
+        return end;
+    }
+
+}
--- src/main/java/com/testcompany/ds/day12/TestingAllSorting.java
+++ src/main/java/com/testcompany/ds/day12/TestingAllSorting.java
@@ -9,21 +9,42 @@ public class TestingAllSorting {
 
         System.out.println("bubble Sort    ="+ Arrays.toString(new AllSorting().bubbleSort(arr)));
 
+        int [] arr11  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
+        System.out.println("bubble Sort    ="+ Arrays.toString(new AllSortingTwo().bubbleSort(arr11)));
+
         int [] arr2  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
 
         System.out.println("insert Sort    ="+ Arrays.toString(new AllSorting().insertSort(arr2)));
+
+        int [] arr22  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
+
+        System.out.println("insert Sort    ="+ Arrays.toString(new AllSortingTwo().insertSort(arr22)));
 
         int [] arr3  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
 
         System.out.println("selection Sort ="+ Arrays.toString(new AllSorting().selection(arr3)));
 
+        int [] arr33  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
+
+        System.out.println("selection Sort ="+ Arrays.toString(new AllSortingTwo().selectionSort(arr33)));
+
         int [] arr4  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
 
         System.out.println("merge Sort     ="+ Arrays.toString(new AllSorting().mergeSort(arr4,0,arr4.length -1)));
 
+
+        int [] arr44  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
+
+        System.out.println("merge Sort     ="+ Arrays.toString(new AllSortingTwo().mergSort(arr4,0,arr4.length -1)));
+
         int [] arr5  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
 
         System.out.println("quick Sort     ="+ Arrays.toString(new AllSorting().quickSort(arr5,0,arr4.length -1)));
+
+        int [] arr55  = new int[]{7,6,10,5,9 ,2 ,1 ,15 ,7};
+
+        System.out.println("quick Sort     ="+ Arrays.toString(new AllSortingTwo().QuickSort(arr5,0,arr4.length -1)));
+
 
     }
 }
