4925df2e083ee1b4c424a68f59bc9bc5d5c77361
Kumar, Amit <Amit.Kumar@cdk.com>
2020-09-06 15:19:52 +0530

    optimize approach for fib memory

---
+++ src/main/java/com/testcompany/ds/day14/TestSorting.java
@@ -0,0 +1,14 @@
+package com.testcompany.ds.day14;
+
+public class TestSorting {
+
+    public static void main(String[] args) {
+
+        FibNaci fibNaci  = new FibNaci(20);
+
+        System.out.println("Fibnaci  - "+fibNaci.fib(8));
+
+        System.out.println("Fibnaci  - "+FibNaci.coutner);
+
+    }
+}
